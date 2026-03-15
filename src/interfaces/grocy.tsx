025ad80f6a39890cce017c6959b4a316c42809b9
Lianna <liannaee@gmail.com>
2026-03-16 00:23:18 +0100

    Additional types for the grocy API wrapper

--- src/interfaces/grocy.tsx
+++ src/interfaces/grocy.tsx
@@ -4,5 +4,9 @@ export type Product = components["schemas"]["Product"]
 export type QuantityUnit = components["schemas"]["QuantityUnit"]
 export type ProductLocation = components["schemas"]["Location"]
 
-export type ObjectByIdEndPointParams = paths["/objects/{entity}/{objectId}"]["parameters"]
-export type ProductBarcodeEndPointParams = paths["/stock/products/by-barcode/{barcode}"]["parameters"]
+export type ObjectByIdParams = paths["/objects/{entity}/{objectId}"]["parameters"]
+export type ProductByBarcodeParams = paths["/stock/products/by-barcode/{barcode}"]["parameters"]
+
+export type ObjectSuccessResponse = paths["/objects/{entity}/{objectId}"]["get"]["responses"]["200"]["content"]["application/json"]
+export type ObjectErrorResponse = paths["/objects/{entity}/{objectId}"]["get"]["responses"]["400"]["content"]["application/json"]
+export type ObjectNotFoundResponse = paths["/objects/{entity}/{objectId}"]["get"]["responses"]["404"]["content"]["application/json"]
