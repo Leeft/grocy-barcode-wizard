5bc90d97d7b932e5708b3eafa50ea828adcef351
Lianna <liannaee@gmail.com>
2026-03-20 12:40:07 +0100

    Interface/types export reformat

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,16 +1,27 @@
-import { name } from "@material-tailwind/react/types/components/select";
-import { Product, ProductLocation } from "./grocy";
-
-
 export interface Option {
   value: string;
   label: string;
 }
 
-export const allBarcodeTypes = [ "product", "grocy-product", "grocy-recipe", "grocy-battery", "bbuddy-operation", "bbuddy-quantity", "sho-operation" ] as const;
-export const specialBarcodeTypes = [ "grocy-recipe", "bbuddy-operation", "bbuddy-quantity", "sho-operation" ] as const;
-export const productOnlyBarcodeTypes = [ "product", "grocy-product" ] as const;
+export const allBarcodeTypes = [
+  "product",
+  "grocy-product",
+  "grocy-recipe",
+  "grocy-battery",
+  "bbuddy-operation",
+  "bbuddy-quantity",
+  "sho-operation",
+] as const;
 
-export type BarcodeAnyType = typeof allBarcodeTypes[number];
-export type BarcodeSpecialType = typeof specialBarcodeTypes[number];
-export type BarcodeProductType = typeof productOnlyBarcodeTypes[number];
+export const specialBarcodeTypes = [
+  "grocy-recipe",
+  "bbuddy-operation",
+  "bbuddy-quantity",
+  "sho-operation",
+] as const;
+
+export const productOnlyBarcodeTypes = ["product", "grocy-product"] as const;
+
+export type BarcodeAnyType = (typeof allBarcodeTypes)[number];
+export type BarcodeSpecialType = (typeof specialBarcodeTypes)[number];
+export type BarcodeProductType = (typeof productOnlyBarcodeTypes)[number];
