ce97239210bf1ba83c3e260909ed7adcb65235f1
Lianna <liannaee@gmail.com>
2026-03-27 13:26:21 +0100

    Clean *npm depencies
    Wasn't using material-tailwind/react anyway due to React 19; might as well clear the warning showing on `pnpm update`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-import withMT from "@material-tailwind/react/utils/withMT";
+//import withMT from "@material-tailwind/react/utils/withMT";
  
-export default withMT({
+export default ({
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
