7e1e5e09d264a6886eb0dd43dc76508613b5cc1f
Lianna <liannaee@gmail.com>
2026-04-12 02:07:36 +0200

    Repair product creation

--- app/forms/quick-product-form-submit.ts
+++ app/forms/quick-product-form-submit.ts
@@ -24,36 +24,16 @@ function dataURLtoFile(dataurl: string, filename: string): File {
   return new File([u8arr], filename, { type: mime });
 }
 
-export async function quickProductFormSubmit( prevstate: unknown, formData: FormData ) {
+export async function quickProductFormSubmit(
+  prevstate: unknown,
+  formData: FormData,
+) {
   const submission = parseWithZod(formData, { schema: QuickProductFormSchema });
 
   // Send the submission back to the client if the status is not successful
-  if (submission.status !== 'success') {
+  if (submission.status !== "success") {
     return submission.reply();
   }
-
-  //const rawFormData = Object.fromEntries(formData.entries());
-  //const validation = FormSchema.safeParse(rawFormData);
-
-  // if (!validation.success) {
-  //   console.error(
-  //     "Form validation errors",
-  //     validation.error.flatten().fieldErrors,
-  //   );
-  //   //console.log("form data state", rawFormData);
-  //   return {
-  //     message: "",
-  //     form: {
-  //       name: "",
-  //       dueOrExpiryDate: "",
-  //       packagingDate: "",
-  //       ...rawFormData,
-  //     },
-  //     errors: validation.error.flatten().fieldErrors,
-  //   };
-  // } else {
-  //   //console.log("validated", validation.data);
-  // }
 
   // validated {
   //   name: 'This is a product name',
@@ -69,56 +49,56 @@ export async function quickProductFormSubmit( prevstate: unknown, formData: Form
   //   defaultDueDaysAfterOpen: 0
   // }
 
-  // let dueDateType = "BEST_BEFORE";
-  // if (validation.data.dueDateType === "expiry-date")
-  //   dueDateType = "EXPIRY_DATE";
-  // else if (validation.data.dueDateType === "no-expiry")
-  //   dueDateType = "NO_EXPIRY";
+  let dueDateType = "BEST_BEFORE";
+  if (submission.value.dueDateType === "expiry-date")
+    dueDateType = "EXPIRY_DATE";
+  else if (submission.value.dueDateType === "no-expiry")
+    dueDateType = "NO_EXPIRY";
 
-  // const canExpire = dueDateType !== DueDateType.NO_EXPIRY;
-  // const data = validation.data;
+  const canExpire = dueDateType !== DueDateType.NO_EXPIRY;
+  const data = submission.value;
 
-  // const queuedProduct = await prisma.product.create({
-  //   data: {
-  //     name: data.name,
-  //     pending: true,
-  //     canBeFrozen: !data.shouldNotBeFrozen,
-  //     unitSystem: data.unitSystem.toUpperCase() as UnitSystem,
-  //     unitAmount: data.mainQuantity,
-  //     unitChosen: data.mainQuantityUnitId,
-  //     dueDateType: dueDateType as DueDateType,
-  //     expiresAt: canExpire ? data.dueOrExpiryDate! : null,
-  //     packagingDate: canExpire ? data.packagingDate! : null,
-  //     defaultDueDays: canExpire ? data.defaultDueDays : null,
-  //     defaultDueDaysAfterOpen: canExpire ? data.defaultDueDaysAfterOpen : null,
-  //     defaultDueDaysAfterFreezing: canExpire
-  //       ? data.defaultDueDaysAfterFreezing
-  //       : null,
-  //     defaultDueDaysAfterThawing: canExpire
-  //       ? data.defaultDueDaysAfterThawing
-  //       : null,
-  //   } as ProductCreateInput,
-  // });
+  const queuedProduct = await prisma.product.create({
+    data: {
+      name: data.name,
+      pending: true,
+      canBeFrozen: !data.shouldNotBeFrozen,
+      unitSystem: data.unitSystem.toUpperCase() as UnitSystem,
+      unitAmount: data.mainQuantity,
+      unitChosen: data.mainQuantityUnitId,
+      dueDateType: dueDateType as DueDateType,
+      expiresAt: canExpire ? data.dueOrExpiryDate! : null,
+      packagingDate: canExpire ? data.packagingDate! : null,
+      defaultDueDays: canExpire ? data.defaultDueDays : null,
+      defaultDueDaysAfterOpen: canExpire ? data.defaultDueDaysAfterOpen : null,
+      defaultDueDaysAfterFreezing: canExpire
+        ? data.defaultDueDaysAfterFreezing
+        : null,
+      defaultDueDaysAfterThawing: canExpire
+        ? data.defaultDueDaysAfterThawing
+        : null,
+    } as ProductCreateInput,
+  });
 
-  // console.log("queued product is", queuedProduct);
+  console.log("queued product is", queuedProduct);
 
-  // if (data.image) {
-  //   const file = dataURLtoFile(data.image, "filename-not-used-yet");
-  //   const arr = new Uint8Array(await file.arrayBuffer());
-  //   await prisma.productPhoto.create({
-  //     data: {
-  //       filename: `capture-${queuedProduct.id}-${Date.now()}.png`,
-  //       data: arr,
-  //       productId: queuedProduct.id,
-  //       grocyFileGroup: "productpictures",
-  //     } as ProductPhotoUncheckedCreateInput,
-  //   });
-  // }
+  if (data.image) {
+    const file = dataURLtoFile(data.image, "filename-not-used-yet");
+    const arr = new Uint8Array(await file.arrayBuffer());
+    await prisma.productPhoto.create({
+      data: {
+        filename: `capture-${queuedProduct.id}-${Date.now()}.png`,
+        data: arr,
+        productId: queuedProduct.id,
+        grocyFileGroup: "productpictures",
+      } as ProductPhotoUncheckedCreateInput,
+    });
+  }
 
-  // await prisma.barcode.update({
-  //   where: { barcode: data.barcode },
-  //   data: { productId: queuedProduct.id },
-  // });
+  await prisma.barcode.update({
+    where: { barcode: data.barcode },
+    data: { productId: queuedProduct.id },
+  });
 
   // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath("/scan");
