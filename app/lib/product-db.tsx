069eba4bf948b116688743ca6e6938c0e80b9429
Lianna <liannaee@gmail.com>
2026-04-16 09:57:24 +0200

    No longer fetch photo data in getProduct call
    It started to give problems passing the data from server to client.
    
    Also:
    
    * Now making use of the NotFoundError error class.
    * Added separate call (yet untested) to fetch photos.

--- app/lib/product-db.tsx
+++ app/lib/product-db.tsx
@@ -1,9 +1,11 @@
 "use server";
 
 import prisma from "@/lib/prisma";
-import { ProductModel } from "@/generated/prisma/models";
+import { NotFoundError } from "@/lib/errors";
 
-export async function getProduct(id: number): Promise<ProductModel> {
+//
+
+export async function getProduct(id: number) {
   "use server";
 
   if (id === undefined) throw new Error("No productId given");
@@ -12,17 +14,22 @@ export async function getProduct(id: number): Promise<ProductModel> {
     where: { id: id },
     include: {
       barcodes: true,
-      productPhoto: true,
+      productPhoto: {
+        omit: {
+          data: true,
+        },
+      },
     },
   });
 
-  if (model === null) throw new Error("Product not found");
+  if (model === null) throw new NotFoundError("Product not found");
 
   return model;
 }
 
 export type GetProduct = Awaited<ReturnType<typeof getProduct>>;
 
+//
 
 export async function getPendingProducts() {
   "use server";
@@ -49,13 +56,13 @@ export async function getPendingProducts() {
 
 export type PendingProducts = Awaited<ReturnType<typeof getPendingProducts>>;
 
+//
 
-export async function getProductsByBarcode(
-  barcode: string,
-) {
+export async function getProductsByBarcode(barcode: string) {
   "use server";
 
-  if (barcode === undefined || barcode.trim() === '') throw new Error("No barcode given");
+  if (barcode === undefined || barcode.trim() === "")
+    throw new Error("No barcode given");
 
   return await prisma.product.findMany({
     select: {
@@ -75,4 +82,24 @@ export async function getProductsByBarcode(
   });
 }
 
-export type ProductsByBarcode = Awaited<ReturnType<typeof getProductsByBarcode>>;
+export type ProductsByBarcode = Awaited<
+  ReturnType<typeof getProductsByBarcode>
+>;
+
+//
+
+export async function getProductPhoto(id: number) {
+  "use server";
+
+  if (id === undefined) throw new Error("No productPhotoId given");
+
+  const model = await prisma.productPhoto.findUnique({
+    where: { id: id },
+  });
+
+  if (model === null) throw new NotFoundError("Product photo not found");
+
+  return model;
+}
+
+export type GetProductPhoto = Awaited<ReturnType<typeof getProductPhoto>>;
