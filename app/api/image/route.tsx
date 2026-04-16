558734bf3ef0774339abb585b185f8117fc9dc00
Lianna <liannaee@gmail.com>
2026-04-16 23:13:01 +0200

    Basic image serving router

---
+++ app/api/image/route.tsx
@@ -0,0 +1,36 @@
+import { getProductPhoto } from "@/lib/product-db";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest): Promise<NextResponse> => {
+  const id = req.nextUrl.searchParams.get("id");
+
+  if (id !== null && id !== undefined) {
+    try {
+      const photo = await getProductPhoto(Number(id));
+      return new NextResponse(photo.data, {
+        status: 200,
+        headers: new Headers({
+          // this optional header triggers a download in the browser
+          "content-disposition": `attachment; filename=${photo.filename}`,
+          "content-type": "image/png",
+          "content-length": photo.data.length + "",
+          // "content-length": stats.size + "",
+        }),
+      });
+    } catch (err: any) {
+      return NextResponse.json(
+        {
+          error: "Error while serving the file: " + err.message,
+        },
+        { status: 500 },
+      );
+    }
+  }
+
+  return NextResponse.json(
+    {
+      error: "Not found",
+    },
+    { status: 500 },
+  );
+};
