1ff275e6305aafa0dd100d6caad1b639633ea7ee
Lianna <liannaee@gmail.com>
2026-04-18 12:12:32 +0200

    QuantityUnitsDropdown can include Abstract options
    It was only WEIGHT, or only VOLUME, or only (ungrouped) ABSTRACT units,
    but now it has an option to include ABSTRACT as a group along with
    either WEIGHT or VOLUME selections.
    
    This is required for selecting units to map to the main unit group
    chosen for the product.

--- app/ui/product/quantity-units-dropdown.tsx
+++ app/ui/product/quantity-units-dropdown.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { QuantityUnit } from "@/interfaces/grocy";
-import React, { useState } from "react";
+import React, { RefObject } from "react";
 import { UnitSystem } from "@/generated/prisma/enums";
 import CustomisableSelect, {
   CustomisableSelectProps,
@@ -16,9 +16,13 @@ const quantityTypes = [
   "volume-us-dry",
 ] as const;
 
-type QuantityType = (typeof quantityTypes)[number];
+const allTypes = ["abstract", ...quantityTypes] as const;
 
-const quantityGroupLabels: Record<QuantityType, string> = {
+type QuantityType = (typeof quantityTypes)[number];
+type AllType = (typeof allTypes)[number];
+
+const quantityGroupLabels: Record<AllType, string> = {
+  abstract: "Abstract/discrete",
   "weight-metric": "Weight (metric)",
   "volume-metric": "Volume (metric)",
   "weight-us": "Weight (US)",
@@ -26,20 +30,31 @@ const quantityGroupLabels: Record<QuantityType, string> = {
   "volume-us-dry": "Volume (US; dry)",
 };
 
-interface QuantityUnitsDropdownProps extends Omit<CustomisableSelectProps, 'options'> {
+interface QuantityUnitsDropdownProps extends Omit<
+  CustomisableSelectProps,
+  "options"
+> {
   units: QuantityUnit[];
   unitSystem: UnitSystem;
-  options?: CustomisableSelectProps['options'];
+  options?: CustomisableSelectProps["options"];
+  allOptions?: boolean;
+  plural?: boolean;
+  ref?: RefObject<HTMLSelectElement>;
 }
 
 export const QuantityUnitsDropdown: React.FC<QuantityUnitsDropdownProps> = ({
   units,
   unitSystem,
+  allOptions = false,
+  plural = false,
+  ref,
   ...rest
 }) => {
   const options = quantityUnitsToOptions({
     units: units,
     unitSystem: unitSystem,
+    allOptions: allOptions,
+    plural: plural,
   });
 
   const pickMe: CustomisableSelectOptionArray = [
@@ -51,42 +66,27 @@ export const QuantityUnitsDropdown: React.FC<QuantityUnitsDropdownProps> = ({
 
   const combinedOptions = pickMe.concat(options);
 
-  const [selectedOption, setSelectedOption] = useState(combinedOptions[0]);
-
-  options.map((value) => {
-    if ("value" in value) {
-      if (Number(value.value) === rest.defaultValue) {
-        setSelectedOption(value);
-      }
-    }
-  });
-
-  // const handleChange = (value: SingleValue<OptionType | GroupType>) => {
-  //   if (
-  //     value !== null &&
-  //     value !== undefined &&
-  //     "value" in value &&
-  //     (selectedId === null || value.value !== selectedId.toString())
-  //   ) {
-  //     setSelectedOption(value);
-  //   }
-  // };
-
-  return (
-    <>
-      <CustomisableSelect {...rest}
-        options={combinedOptions}
-      />
-    </>
-  );
+  return <CustomisableSelect {...rest} ref={ref} options={combinedOptions} />;
 };
+
+function compareWords(a: QuantityUnit, b: QuantityUnit) {
+  if (a.name!.toLowerCase() < b.name!.toLowerCase()) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
 
 function quantityUnitsToOptions({
   units,
   unitSystem,
+  allOptions,
+  plural,
 }: {
   units: QuantityUnit[];
   unitSystem: UnitSystem;
+  allOptions: boolean;
+  plural: boolean;
 }) {
   if (units === undefined) return [];
 
@@ -95,7 +95,7 @@ function quantityUnitsToOptions({
   if (unitSystem !== UnitSystem.ABSTRACT) {
     quantityTypes.forEach((type: QuantityType) => {
       const groupOptions: CustomisableSelectOptionArray = [];
-      units.forEach((entity: QuantityUnit) => {
+      units.sort(compareWords).forEach((entity: QuantityUnit) => {
         if (
           entity !== undefined &&
           entity.id &&
@@ -107,27 +107,49 @@ function quantityUnitsToOptions({
           if (entity.userfields.type == type) {
             groupOptions.push({
               value: entity.id.toString(),
-              label: entity.name!,
+              label: plural ? entity.name_plural! : entity.name!,
               type: entity.userfields.type,
             });
           }
         }
       });
 
-      options.push({
-        label: quantityGroupLabels[type],
-        options: groupOptions,
-      });
+      if (groupOptions.length > 0) {
+        options.push({
+          label: quantityGroupLabels[type],
+          options: groupOptions,
+        });
+      }
     });
-  } else {
-    function compareWords(a: QuantityUnit, b: QuantityUnit) {
-      if (a.name!.toLowerCase() < b.name!.toLowerCase()) {
-        return -1;
-      } else {
-        return 1;
+
+    if (allOptions) {
+      const abstractOptions: CustomisableSelectOptionArray = [];
+
+      units.forEach((entity: QuantityUnit) => {
+        if (
+          entity !== undefined &&
+          entity.id &&
+          entity.userfields !== undefined &&
+          (entity.userfields.type === undefined ||
+            entity.userfields.type === null ||
+            entity.userfields.type === "")
+        ) {
+          abstractOptions.push({
+            value: entity.id.toString(),
+            label: entity.name!,
+            type: entity.userfields.type,
+          });
+        }
+      });
+
+      if (abstractOptions.length > 0) {
+        options.unshift({
+          label: quantityGroupLabels["abstract"],
+          options: abstractOptions,
+        });
       }
     }
-
+  } else {
     units.sort(compareWords).forEach((entity: QuantityUnit) => {
       if (
         entity !== undefined &&
