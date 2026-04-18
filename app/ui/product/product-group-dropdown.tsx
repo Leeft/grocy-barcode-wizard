10e550f673102c3e1e0a6218ed1858fd0eabc0d9
Lianna <liannaee@gmail.com>
2026-04-18 12:10:26 +0200

    ProductGroupDropdown update
    This one got missed in all the other dropdown updates. Now matches
    the same inheritance pattern as the other dropdowns.

--- app/ui/product/product-group-dropdown.tsx
+++ app/ui/product/product-group-dropdown.tsx
@@ -2,72 +2,45 @@
 
 import React from "react";
 import { ProductGroup } from "@/interfaces/grocy";
-import CustomSelect from "../custom-select";
-import { OptionType } from "@/interfaces/options";
-import { SingleValue } from "react-select";
+import CustomisableSelect, {
+  CustomisableSelectOption,
+  CustomisableSelectOptionArray,
+  CustomisableSelectProps,
+} from "../customisable-select";
 
-export function ProductGroupDropdown({
-  name,
-  units,
-  className,
-  setSelectedId,
-  required = false,
-  insert,
-  placeholder = "Select or search for a product category...",
-}: {
-  name: string;
+interface ProductGroupDropdownProps extends Omit<
+  CustomisableSelectProps,
+  "options"
+> {
   units: ProductGroup[];
-  className?: string;
-  setSelectedId?: React.Dispatch<React.SetStateAction<number>>;
-  required?: boolean;
-  insert?: OptionType;
-  placeholder?: string;
-}) {
+  insert?: CustomisableSelectOption;
+  options?: CustomisableSelectProps["options"];
+}
+
+export const ProductGroupDropdown: React.FC<ProductGroupDropdownProps> = ({
+  units,
+  insert,
+  ...rest
+}) => {
   const options = productGroupsToOptions({
     entityObjects: units,
   });
 
-  let defaultValue = undefined;
-
-  if (!required && insert !== undefined) {
+  if (!rest.required && insert !== undefined) {
     options.unshift(insert);
-    defaultValue = options[0];
   }
 
-  return (
-    <CustomSelect
-      className={className}
-      maxMenuHeight={500}
-      name={name}
-      options={options}
-      onChange={(
-        inputValue: SingleValue<OptionType> /*action: ActionMeta<Option>*/,
-      ) => {
-        if (setSelectedId && inputValue?.value !== undefined)
-          setSelectedId(Number.parseInt(inputValue?.value));
-      }}
-      defaultValue={defaultValue}
-      isSearchable
-      required={required}
-      placeholder={placeholder}
-      noOptionsMessage={({ inputValue }) =>
-        inputValue
-          ? `No product groups found for "${inputValue}"`
-          : "Start typing to pick..."
-      }
-      loadingMessage={() => "Loading..."}
-    />
-  );
-}
+  return <CustomisableSelect {...rest} options={options} />;
+};
 
 function productGroupsToOptions({
   entityObjects,
 }: {
   entityObjects: ProductGroup[];
-}): OptionType[] {
+}): CustomisableSelectOptionArray {
   if (entityObjects === undefined) return [];
 
-  const options: OptionType[] = [];
+  const options: CustomisableSelectOptionArray = [];
 
   function compareWords(a: ProductGroup, b: ProductGroup) {
     if (a.name!.toLowerCase() < b.name!.toLowerCase()) {
