fc87186f0d0faa3888ed6c6404d8ed93c92e9f89
Lianna <liannaee@gmail.com>
2026-05-08 17:06:11 +0200

    Replace last heroicon with lucide

--- app/ui/tooltip-wrapper.tsx
+++ app/ui/tooltip-wrapper.tsx
@@ -1,4 +1,4 @@
-import { InformationCircleIcon } from "@heroicons/react/20/solid";
+import { Info } from "lucide-react";
 import React, { ReactNode } from "react";
 import { Tooltip } from "react-tooltip";
 
@@ -12,12 +12,9 @@ const TooltipWrapper: React.FC<TextWrapperProps> = ({ id, children, className })
   return (
     <div className={`static inline-block h-1.25 w-1.25 ${className}`}>
       <span className="h-1.25 w-1.25" onClick={(e) => e.preventDefault()}>
-        <a
-          className="relative top-0 inline-block w-10 cursor-help pl-1"
-          data-tooltip-id={id}
-        >
+        <a className="relative top-0 inline-block w-10 cursor-help pl-1" data-tooltip-id={id}>
           &nbsp;
-          <InformationCircleIcon className="inline size-5 text-slate-300" />
+          <Info className="inline size-5 text-form-label/50 mt-[-2]" />
         </a>
         <Tooltip id={id} className="info-tooltip">
           {children}
