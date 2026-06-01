b1f4245831dd23af652cee7bfa1d2531f2d89295
Lianna <liannaee@gmail.com>
2026-06-01 12:02:01 +0200

    Tidy interfaces/grocy.ts

--- app/interfaces/grocy.ts
+++ app/interfaces/grocy.ts
@@ -1,6 +1,9 @@
 import { DueDateType, PurchasePriceType, StockLabelType } from "@/generated/prisma/enums";
 import type { paths, components } from "./grocy.d";
 
+// The API schema provided Product schema is missing various columns, causing
+// typescript validation issues. Extending the generated type here to also add
+// the missing columns.
 export type Product = components["schemas"]["Product"] & {
   default_best_before_days_after_freezing?: number;
   default_best_before_days_after_thawing?: number;
@@ -44,40 +47,6 @@ export type Error500 = components["schemas"]["Error500"];
 // 200 status of /stock/products/{productId}/inventory
 // 200 status of /stock/products/{productId}/open
 export type StockLogEntry = components["schemas"]["StockLogEntry"];
-
-// CREATE TABLE products (
-//         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-//         name TEXT NOT NULL UNIQUE,
-//         description TEXT,
-//         product_group_id INTEGER,
-//         active TINYINT NOT NULL DEFAULT 1 CHECK(active IN (0, 1)),
-//         location_id INTEGER NOT NULL,
-//         shopping_location_id INTEGER,
-//         qu_id_purchase INTEGER NOT NULL,
-//         qu_id_stock INTEGER NOT NULL,
-//         min_stock_amount INTEGER NOT NULL DEFAULT 0,
-//         default_best_before_days INTEGER NOT NULL DEFAULT 0,
-//         default_best_before_days_after_open INTEGER NOT NULL DEFAULT 0,
-//         default_best_before_days_after_freezing INTEGER NOT NULL DEFAULT 0,
-//         default_best_before_days_after_thawing INTEGER NOT NULL DEFAULT 0,
-//         picture_file_name TEXT,
-//         enable_tare_weight_handling TINYINT NOT NULL DEFAULT 0,
-//         tare_weight REAL NOT NULL DEFAULT 0,
-//         not_check_stock_fulfillment_for_recipes TINYINT DEFAULT 0,
-//         parent_product_id INT,
-//         calories INTEGER,
-//         cumulate_min_stock_amount_of_sub_products TINYINT DEFAULT 0,
-//         due_type TINYINT NOT NULL DEFAULT 1 CHECK(due_type IN (1, 2)),
-//         quick_consume_amount REAL NOT NULL DEFAULT 1,
-//         hide_on_stock_overview TINYINT NOT NULL DEFAULT 0 CHECK(hide_on_stock_overview IN (0, 1)),
-//         default_stock_label_type INTEGER NOT NULL DEFAULT 0,
-//         should_not_be_frozen TINYINT NOT NULL DEFAULT 0 CHECK(should_not_be_frozen IN (0, 1)),
-//         treat_opened_as_out_of_stock TINYINT NOT NULL DEFAULT 1 CHECK(treat_opened_as_out_of_stock IN (0, 1)),
-//         no_own_stock TINYINT NOT NULL DEFAULT 0 CHECK(no_own_stock IN (0, 1)),
-//         default_consume_location_id INTEGER,
-//         move_on_open TINYINT NOT NULL DEFAULT 0 CHECK(move_on_open IN (0, 1)),
-//         row_created_timestamp DATETIME DEFAULT (datetime('now', 'localtime'))
-// , qu_id_consume INTEGER, auto_reprint_stock_label TINYINT NOT NULL DEFAULT 0 CHECK(auto_reprint_stock_label IN (0, 1)), quick_open_amount REAL NOT NULL DEFAULT 1, qu_id_price INTEGER, disable_open TINYINT NOT NULL DEFAULT 0 CHECK(disable_open IN (0, 1)), default_purchase_price_type TINYINT NOT NULL DEFAULT 1 CHECK(default_purchase_price_type IN (1, 2, 3)));
 
 export type QuantityUnit = components["schemas"]["QuantityUnit"];
 export type ProductLocation = components["schemas"]["Location"] & {
